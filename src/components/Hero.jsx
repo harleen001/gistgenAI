e37b0a52fea4db54a4710c4cd93d8510a9212ba4
harleen <harleen.johal31@gmail.com>
2024-07-11 20:33:14 +0530

    logo-text-fixed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -21,7 +21,7 @@ const Hero = () => {
 
       <h1 className='head_text'>
         Digest Articles with <br className='max-md:hidden' />
-        <span className='orange_gradient '>gistgen</span>
+        <span className='orange_gradient '>GistGen</span>
         <span className='orange_gradient2 '>AI</span>
       </h1>
       <h2 className='desc'>
